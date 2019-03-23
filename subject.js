e908dad07e2ac6efbba72e51f2598f0154607996
TearTheSky (Masumi Yoshida) <the.everglow.m.a.e@gmail.com>
2019-03-23 16:57:28 +0900

    add feature of selecting and finishing subjects

--- subject.js
+++ subject.js
@@ -4,25 +4,50 @@ function pressEnterKey(){
     }
 }
 function addSubject(){
-        register = document.getElementById('register');
-        talkingNow = document.getElementById('talkingNow');
-        window.alert("お題を登録します：" + register.value);
+    register = document.getElementById('register');
+    talkingNow = document.getElementById('talkingNow');
+    window.alert("お題を登録します：" + register.value);
 
-        newSubjectSelectButton = document.createElement("input");
-        newSubjectSelectButton.setAttribute('type', 'radio');
-        newSubjectSelectButton.setAttribute('name', 'registeredSubjects');
-        newSubjectSelectButton.value = register.value;
-        document.getElementById('subjectList').appendChild(newSubjectSelectButton);
-        newSubjectTitle =document.createTextNode(register.value);
-        document.getElementById('subjectList').appendChild(newSubjectTitle);
-        newLineBrake = document.createElement("br");
-        document.getElementById('subjectList').appendChild(newLineBrake);
-        // talkingNow.innerHTML = register.value;
-        register.value='';
+    newSubjectSelectButton = document.createElement("input");
+    newSubjectSelectButton.setAttribute('type', 'radio');
+    newSubjectSelectButton.setAttribute('name', 'registeredSubjects');
+    newSubjectSelectButton.value = register.value;
+    document.getElementById('subjectList').appendChild(newSubjectSelectButton);
+
+    newSubjectTitle = document.createTextNode(register.value);
+    document.getElementById('subjectList').appendChild(newSubjectTitle);
+    newLineBrake = document.createElement("br");
+    document.getElementById('subjectList').appendChild(newLineBrake);
+    register.value = '';
 }
 
 function decideSubject(){
-    selectedSubject = document.getElementById('register');
     talkingNow = document.getElementById('talkingNow');
-    talkingNow.innerHTML = register.value;
+
+    var element = document.getElementById('subjectList');
+    var radioNodeList = element.registeredSubjects;
+
+    if (radioNodeList.value === undefined){
+
+    } else {
+        var choseSubject = radioNodeList.value;
+    }
+
+    if ( choseSubject === "" ) {
+        window.alert("お題はまだ選択されていないようです。\n登録しましたか？");
+        return;
+    } else {
+    	// aには選択状態の値が代入されている
+        talkingNow.innerHTML = choseSubject;
+    }
+
+}
+
+function finishSubject(){
+    talkingNow = document.getElementById('talkingNow');
+
+    newFinishedSubject = document.createElement("p");
+    newFinishedSubject.innerHTML = talkingNow.innerHTML
+    document.getElementById('finishedSubjectList').appendChild(newFinishedSubject);
+    talkingNow.innerHTML='';
 }
