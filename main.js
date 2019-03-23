41b09804dcdf5e0a16cdc563cf0684f41bf437fc
TearTheSky (Masumi Yoshida) <the.everglow.m.a.e@gmail.com>
2019-03-23 21:30:31 +0900

    fix design it is so nice

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
 
   // ブラウザ(Chromium)の起動, 初期画面のロード
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 500, height: 900});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   mainWindow.on('closed', function() {
