e9896107f719bb10f8b57c10d86f5d5fc0a46f74
TearTheSky (Masumi Yoshida) <the.everglow.m.a.e@gmail.com>
2019-03-23 20:27:46 +0900

    update we use this aplication stylish

--- subject.js
+++ subject.js
@@ -8,19 +8,35 @@ function addSubject(){
     talkingNow = document.getElementById('talkingNow');
     window.alert("お題を登録します：" + register.value);
 
-    newSubjectSelectButtonPlace = document.createElement("div");
-    newSubjectSelectButtonPlaceId = "part-of-" + register.value;
+    //<li>タグを追加しここにラジオボタンを入れる
+    newSubjectSelectButtonPlace = document.createElement("li");
+    newSubjectSelectButtonPlaceId = "li-id-of-" + register.value;
     newSubjectSelectButtonPlace.setAttribute('id', newSubjectSelectButtonPlaceId);
     document.getElementById('subjectList').appendChild(newSubjectSelectButtonPlace);
 
+    // <input>タグの追加
     newSubjectSelectButton = document.createElement("input");
+    newSubjectSelectButtonId = "button-id-of-" + register.value;
     newSubjectSelectButton.setAttribute('type', 'radio');
+    newSubjectSelectButton.setAttribute('id', newSubjectSelectButtonId);
+    newSubjectSelectButton.setAttribute('class', 'radio-inline__input');
     newSubjectSelectButton.setAttribute('name', 'registeredSubjects');
     newSubjectSelectButton.value = register.value;
     document.getElementById(newSubjectSelectButtonPlaceId).appendChild(newSubjectSelectButton);
 
-    newSubjectTitle = document.createTextNode(register.value);
-    document.getElementById(newSubjectSelectButtonPlaceId).appendChild(newSubjectTitle);
+    // テキストの追加
+    newSubjectSelectButtonLabel = document.createElement("label");
+    newSubjectSelectButtonLabel.setAttribute('class', 'radio-inline__label');
+    newSubjectSelectButtonLabel.setAttribute('for', newSubjectSelectButtonId);
+    newSubjectSelectButtonLabel.value = register.value;
+    newSubjectSelectButtonLabel.innerHTML = register.value;
+    document.getElementById(newSubjectSelectButtonPlaceId).appendChild(newSubjectSelectButtonLabel);
+
+
+    // ラベルのテキストを挿入
+    //newSubjectSelectButtonLabel.innerHTML = register.value;
+
+    // <br> の追加
     newLineBrake = document.createElement("br");
     document.getElementById(newSubjectSelectButtonPlaceId).appendChild(newLineBrake);
     register.value = '';
@@ -28,7 +44,7 @@ function addSubject(){
 
 function decideSubject(){
     talkingNow = document.getElementById('talkingNow');
-    element = document.getElementById('subjectList');
+    element = document.getElementById('subjectForm');
 
     if (element.registeredSubjects === undefined){
         window.alert("お題はまだ選択されていないようです。\n登録しましたか？");
@@ -44,7 +60,7 @@ function decideSubject(){
     } else {
     	// aには選択状態の値が代入されている
         talkingNow.innerHTML = choseSubject;
-        choseSubjectParentId = 'part-of-' + choseSubject
+        choseSubjectParentId = 'li-id-of-' + choseSubject
         choseSubjectParent = document.getElementById(choseSubjectParentId);
         choseSubjectParent.parentNode.removeChild(choseSubjectParent);
     }
@@ -53,7 +69,6 @@ function decideSubject(){
 
 function finishSubject(){
     talkingNow = document.getElementById('talkingNow');
-
     newFinishedSubject = document.createElement("p");
     newFinishedSubject.innerHTML = talkingNow.innerHTML
     document.getElementById('finishedSubjectList').appendChild(newFinishedSubject);
